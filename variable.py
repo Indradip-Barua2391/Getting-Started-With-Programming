dea4559f9260284f1ad0143ff5022a80bfb205eb
Indradip <chadsigmarizzler@gmail.com>
2025-01-18 20:39:25 +0600

    first commit

---
+++ variable.py
@@ -0,0 +1,12 @@
+name = "Indradip Barua"
+
+age = 13
+
+friend = "GigaPepaDora"
+
+
+print("My name is", name, " and my age is ", age, "\nand my best friends name is ", friend)
+
+print()
+
+print(f"My name is {name} and my age is {age}. \nand my Best friends name is {friend}")
---
+++ welcome.py
@@ -0,0 +1,6 @@
+print("Welcome to the world of Programming with Python!!")
+print(15 + 45)
+print("15" + "45")
+
+print("My Name is Indradip Barua, And my age is ", 20+8,".And my weight is ", 36, "(Probably)")
+print(f"My name is Indradip, and my age is {20+8} and my weight is {36} ")
