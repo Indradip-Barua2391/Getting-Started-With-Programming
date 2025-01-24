550f6dfbd21e59f55a1ea380ba01ac2317b9a1b1
Indradip <chadsigmarizzler@gmail.com>
2025-01-24 14:34:11 +0600

    abc

--- variable.py
+++ variable.py
@@ -1,12 +1,31 @@
-name = "Indradip Barua"
+name1 = "Siyaf"
 
-age = 13
-
-friend = "GigaPepaDora"
+age1= 12
 
 
-print("My name is", name, " and my age is ", age, "\nand my best friends name is ", friend)
 
-print()
 
-print(f"My name is {name} and my age is {age}. \nand my Best friends name is {friend}")
+
+print(f"My first friend's name is {name1} and his age is  {age1} years old.")
+
+name2 = "Wahib"
+age2 = 15
+
+print(f"My Second friend's name is {name2} and his age is {age2}years old.")
+
+name3 = "Ahnaf Araf"
+age3 = 16
+
+print(f"My third friend's name is {name3} and his age is {age3} years old.")
+
+name4 = "Abidur"
+age4 = 15
+
+print(f"My fourth friend's name is {name4} and his age is {age4} years old")
+
+name5 = "Ridhan"
+age5 = 13
+
+print(f"My Fifth friend's name is {name5} and his age is {age5} years old")
+
+
